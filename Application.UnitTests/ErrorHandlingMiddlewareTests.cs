40ac8df76886dd192171287e6a1a9a9b5caefa80
ShiroTsuma <spam.shiro@gmail.com>
2026-07-11 11:41:32 +0200

    fix: hide duplicate book internal id

---
+++ Application.UnitTests/ErrorHandlingMiddlewareTests.cs
@@ -0,0 +1,33 @@
+using System.Text.Json;
+using Domain.Entities;
+using Domain.Exceptions;
+using Infrastructure.Middleware;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Application.UnitTests;
+
+public class ErrorHandlingMiddlewareTests
+{
+    [Fact]
+    public async Task InvokeAsync_ShouldHideBookConflictInternalId()
+    {
+        var existingId = Guid.Parse("11111111-1111-1111-1111-111111111111");
+        var context = new DefaultHttpContext();
+        context.Response.Body = new MemoryStream();
+        var middleware = new ErrorHandlingMiddleware(
+            _ => throw new EntityAlreadyExistsException<Book, Guid>("Duplicated Book", existingId),
+            Mock.Of<ILogger<ErrorHandlingMiddleware>>());
+
+        await middleware.InvokeAsync(context);
+
+        context.Response.Body.Position = 0;
+        using var document = await JsonDocument.ParseAsync(context.Response.Body);
+        var detail = document.RootElement.GetProperty("detail").GetString();
+
+        Assert.Equal(StatusCodes.Status409Conflict, context.Response.StatusCode);
+        Assert.Equal("A book named 'Duplicated Book' already exists.", detail);
+        Assert.DoesNotContain(existingId.ToString(), detail);
+    }
+}
